bcd6dde1ac666f1c8d755a9daba8c44c7f0306da
LE DOAN HIEU <152629519+OTISDOAN@users.noreply.github.com>
2025-02-08 09:58:57 +0700

    update

--- src/components/Signup/Accounts.jsx
+++ src/components/Signup/Accounts.jsx
@@ -3,14 +3,14 @@ import { FaApple } from "react-icons/fa";
 function Accounts() {
   return (
     <>
-      <div className="flex justify-between mt-[60px] mb-[20px] w-[100%] gap-x-[1rem]">
-        <button className="flex items-center justify-center border-[#ddd] border-[1px] p-[5px] rounded-[10px] px-[10px] w-[50%] hover:bg-[#3a5b22] hover:text-white">
-          <FaGoogle />
-          <p className="ml-[5px]">Sign in with Google</p>
+      <div className="flex justify-between mt-[60px] mb-[20px] w-[100%] lg:flex-row flex-col gap-x-4 gap-y-4">
+        <button className="flex items-center justify-center border-[#ddd] border-[1px] p-[5px] rounded-[10px] px-[10px] w-[100%] lg:w-[50%] hover:bg-[#3a5b22] hover:text-white">
+          <FaGoogle className="text-[1.4rem]"/>
+          <p className="ml-[10px] text-[0.9rem]">Sign in with Google</p>
         </button>
-        <button className="flex items-center justify-center border-[#ddd] border-[1px] p-[5px] rounded-[10px] px-[20px] w-[50%] hover:bg-[#3a5b22] hover:text-white">
-          <FaApple />
-          <p className="ml-[5px]">Sign in with Apple</p>
+        <button className="flex items-center justify-center border-[#ddd] border-[1px] p-[5px] rounded-[10px] px-[20px] w-[100%] lg:w-[50%] hover:bg-[#3a5b22] hover:text-white">
+          <FaApple className="text-[1.6rem]"/>
+          <p className="ml-[10px] text-[0.9rem]">Sign in with Apple</p>
         </button>
       </div>
       <div className="text-center w-[100%]">
